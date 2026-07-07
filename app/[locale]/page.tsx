bda6afa9c6f3c55d092e918a267fbaf246bd5856
Gabriel RZ <gabriel96x1@gmail.com>
2026-07-07 11:28:57 -0600

    Removed services until ready

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -33,7 +33,7 @@ export default async function Home({ params }: HomeProps) {
     <main id="main-content">
       <HeroSection />
       <WhatIDoSection />
-      <ServicesSection />
+      {/* <ServicesSection /> TODO: Add services section when ready  */}
       <PortfolioSection />
       <BlogPreviewSection />
       <AboutSection />
